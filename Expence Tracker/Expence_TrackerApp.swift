fe6bbd635883418d22711124f1b0a8f7e68924d2
Hsiur <ruish10@me.com>
2024-01-05 10:26:43 +0500

    Initial Commit

---
+++ Expence Tracker/Expence_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expence_TrackerApp.swift
+//  Expence Tracker
+//
+//  Created by Руслан Ишмухаметов on 05.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct Expence_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
